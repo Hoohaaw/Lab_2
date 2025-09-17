6d51e40797a295f15ca0cb7db5053bf46c681124
Alex Palm <ap224gy@student.lnu.se>
2025-09-17 09:46:41 +0200

    Add return statements for all functions in app.js

--- password-validator/src/app.js
+++ password-validator/src/app.js
@@ -20,8 +20,7 @@ class PasswordValidator {
     }
     if (str.length < minLength || str.length > maxLength) {
       throw new RangeError(`String length must be between ${minLength} and ${maxLength}`);
-    }
-    else {
+    } else {
       return true;
     }
   }
@@ -29,29 +28,39 @@ class PasswordValidator {
   containsUppercase(str) {
     if (str ==! /[A-Z]/.test(str)) {
       throw new Error("String must contain at least one uppercase letter");
-    } 
+    } else {
+      return true;
+    }
   }
 
   containsLowercase(str) {
     if (str ==! /[a-z]/.test(str)) {
       throw new Error("String must contain at least one lowercase letter");
+    } else {
+      return true;
     }
   }
   containsDigit(str) {
     if (str ==! /\d/.test(str)) {
       throw new Error("String must contain at least one digit letter");
+    } else {
+      return true;
     }
   }
 
   containsSpecialChar(str) {
     if (str ==! /[!@#$%^&*(),.?":{}|<>]/.test(str)) {
       throw new Error("String must contain at least one special character");
+    } else {
+      return true;
     }
   }
 
   doesNotContainWhitespace(str) {
     if (str ==! /\s/.test(str)) {
       throw new Error("String must not contain whitespace characters");
+    } else {
+      return true;
     }
   }
 
@@ -59,7 +68,7 @@ class PasswordValidator {
     const firstChar = str[0];
     for (let i = 1; i < str.length; i++) {
       if (str[i] !== firstChar) {
-        return;
+        return true;
       }
     }
     throw new Error("String must not contain just one letter");
@@ -68,9 +77,11 @@ class PasswordValidator {
   passwordEqualToUsername(password, username) {
     if (!username) {
       throw new Error("Username must be sent as argument");
-    }
+    } 
     if (password === username) {
       throw new Error("Password must not be equal to username");
+    }  else {
+      return true;
     }
   }
 }
