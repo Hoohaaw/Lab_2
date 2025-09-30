4d0e0143f3d4d3db750b1cd298e7ba6d657e3f58
Alex Palm <ap224gy@student.lnu.se>
2025-09-30 15:53:13 +0200

    Clear some strings in sequence

--- password-validator/src/sequentialLetters.js
+++ password-validator/src/sequentialLetters.js
@@ -5,25 +5,25 @@ class SequentialLetters {
   getListOfSequentialLetters() {
     return [
     // Alphabet forward
-      "abc", "abcd", "abcde", "abcdef", "abcdefg", "abcdefgh", "abcdefghi", "abcdefghij",
-      "klm", "klmn", "klmno", "klmnop", "klmnopq", "klmnopqr", "klmnopqrs", "klmnopqrst",
-      "xyz", "wxyz",
+      "abcd", "abcde", "abcdef", "abcdefg", "abcdefgh", "abcdefghi", "abcdefghij",
+      "klmn", "klmno", "klmnop", "klmnopq", "klmnopqr", "klmnopqrs", "klmnopqrst",
+      "wxyz",
 
       // Alphabet backward
-      "cba", "dcba", "edcba", "fedcba", "gfedcba",
-      "zyx", "zyxw", "zyxwv", "zyxwvu", "zyxwvut",
+      "dcba", "edcba", "fedcba", "gfedcba",
+      "zyxw", "zyxwv", "zyxwvu", "zyxwvut",
 
       // Numbers forward
-      "123", "1234", "12345", "123456", "1234567", "12345678", "123456789",
+      "1234", "12345", "123456", "1234567", "12345678", "123456789",
       "2345", "3456", "4567", "5678", "6789",
 
       // Numbers backward
-      "321", "4321", "54321", "654321", "7654321", "87654321", "987654321",
+      "4321", "54321", "654321", "7654321", "87654321", "987654321",
 
       // Common keyboard patterns (QWERTY)
-      "qwe", "qwer", "qwert", "qwerty",
+      "qwer", "qwert", "qwerty",
       "asdf", "asdfg", "asdfgh",
-      "zxc", "zxcv", "zxcvb",
+      "zxcv", "zxcvb",
       "poiuy", "lkjhg", "mnbv",
     ];
   }
